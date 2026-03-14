4459d1ef0d1dd3270f744b9976fe3c74d6d2fabc
Mei Matteo <teomei68@gmail.com>
2026-03-14 08:36:56 +0100

    Experiment with macros for removing the factory objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <chrono>
-#include <vector>
 #include <iostream>
 #include "problem.h"
 #include "solvers/sparse.h"
@@ -9,18 +8,13 @@
 #define TIC() std::chrono::high_resolution_clock::now()
 
 
+
+
 int main(int argc, char* argv[]) {
 	int MAX_PROBLEM_SIZE = 20'000;
 	if (argc > 1) {
 		MAX_PROBLEM_SIZE = std::stoi(argv[1]);
 	}
-
-	std::vector<std::unique_ptr<SolverFactory>> solver_factories;
-	solver_factories.push_back(std::make_unique<Stencil>(3));
-	solver_factories.push_back(std::make_unique<Stencil>(5));
-	solver_factories.push_back(std::make_unique<Stencil>(7));
-	solver_factories.push_back(std::make_unique<Stencil>(9));
-	solver_factories.push_back(std::make_unique<Tridiagonal>());
 
 	std::cout << "n,name,errnorm,resnorm,assemble_time_s,solve_time_s,total_time_s" << std::endl;
 
@@ -31,37 +25,39 @@ int main(int argc, char* argv[]) {
 		Eigen::VectorXd res(n);
 		Eigen::VectorXd x(n);
 
-		for (const auto& factory : solver_factories) {
-			const auto assemble_start_time = TIC();
-			std::unique_ptr<Solver> solver = factory->generate_solver(n);
-			const auto assemble_end_time = TIC();
-			const std::chrono::duration<double> assemble_time = assemble_end_time - assemble_start_time;
+		#define PROFILE_SOLVER(solver_recipe) do {                                                           \
+			const auto assemble_start_time = TIC();                                                      \
+			auto solver = solver_recipe;                                                                 \
+			const auto assemble_end_time = TIC();                                                        \
+			const std::chrono::duration<double> assemble_time = assemble_end_time - assemble_start_time; \
+		                                                                                                     \
+			const auto solve_start_time = TIC();                                                         \
+			solver.solve(x, rhs);                                                                        \
+			const auto solve_end_time = TIC();                                                           \
+			const std::chrono::duration<double> solve_time = solve_end_time - solve_start_time;          \
+		                                                                                                     \
+			const std::chrono::duration<double> total_time = assemble_time + solve_time;                 \
+		                                                                                                     \
+			solver.residual(res, x, rhs);                                                                \
+		                                                                                                     \
+			std::cout                                                                                    \
+				<< n                                                                                 \
+				<< ','                                                                               \
+				<< "solver_recipe"                                                                   \
+				<< ','                                                                               \
+				<< (sol - x).norm()                                                                  \
+				<< ','                                                                               \
+				<< res.norm()                                                                        \
+				<< ','                                                                               \
+				<< assemble_time.count()                                                             \
+				<< ','                                                                               \
+				<< solve_time.count()                                                                \
+				<< ','                                                                               \
+				<< total_time.count()                                                                \
+				<< std::endl;                                                                        \
+		} while (0)                                                                                          \
 
-			const auto solve_start_time = TIC();
-			solver->solve(x, rhs);
-			const auto solve_end_time = TIC();
-			const std::chrono::duration<double> solve_time = solve_end_time - solve_start_time;
-
-			const std::chrono::duration<double> total_time = assemble_time + solve_time;
-
-			solver->residual(res, x, rhs);
-
-			std::cout
-				<< n
-				<< ','
-				<< factory->name()
-				<< ','
-				<< (sol - x).norm()
-				<< ','
-				<< res.norm()
-				<< ','
-				<< assemble_time.count()
-				<< ','
-				<< solve_time.count()
-				<< ','
-				<< total_time.count()
-				<< std::endl;
-		}
+		PROFILE_SOLVER(TridiagonalSolver(n));
 	}
 
 	return 0;
