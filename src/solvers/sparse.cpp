f9ef9a243c6dc9f20da552338f203414edca490a
Mei Matteo <teomei68@gmail.com>
2026-03-21 08:52:48 +0100

    refactor: improve matrix assembly procedure

--- src/solvers/sparse.cpp
+++ src/solvers/sparse.cpp
@@ -1,8 +1,13 @@
 #include "solvers/sparse.h"
+#include <cassert>
 
 
 static Eigen::SparseMatrix<double> assemble_system_matrix(int n, const Eigen::MatrixXd& laplacian_stencils) {
-	const int nodes = laplacian_stencils.cols();
+	assert(laplacian_stencils.rows() == laplacian_stencils.cols());
+	assert(n > laplacian_stencils.cols());
+
+	const int m = laplacian_stencils.cols();
+
 	const double h = 1.0 / (n - 1.0);
 
 	// the original laplacian stencils are modified to limit the calls to
@@ -10,32 +15,41 @@ static Eigen::SparseMatrix<double> assemble_system_matrix(int n, const Eigen::Ma
 	//
 	// as it's an expensive operation
 	Eigen::MatrixXd operator_stencils(laplacian_stencils);
-	for (int i = 0; i < nodes; ++i) {
+	for (int i = 0; i < m; ++i) {
 		operator_stencils(i,i) += h*h;
 	}
 
 	Eigen::SparseMatrix<double> A(n,n);
-	A.reserve(Eigen::VectorXi::Constant(n, nodes));
+	A.reserve(Eigen::VectorXi::Constant(n, m));
 
 	A.insert(0,0) = 1.0;
 
 	// we are trying to assemble the matrix in a column-major format
 	// this is a gateway to directly produce the compressed data structures
 	for (int i = 1; i < n-1; ++i) {
-		int centerPos;
+		int lb, ub;
 
-		if (i < nodes/2) {
-			centerPos = i;
-		}
-		else if (n-1-i < nodes/2) {
-			centerPos = nodes - (n-1-i);
-		}
-		else {
-			centerPos = nodes/2;
+		const int left_neighbours = i;
+		const int right_neighbours = n-1-i;
+
+		if (left_neighbours < m/2) {
+			lb = 0;
+			ub = m;
+		} else if (right_neighbours < m/2) {
+			ub = n;
+			lb = n-m;
+		} else {
+			lb = i - m/2;
+			ub = lb + m;
 		}
 
-		for (int j = 0; j < nodes; ++j) {
-			A.insert(i, i-centerPos+j) = operator_stencils(centerPos,j);
+		for (int j = lb; j < ub; ++j) {
+			assert(j >= 0);
+			assert(j < n);
+			assert(i-lb >= 0);
+			assert(i-lb < m);
+
+			A.insert(i,j) = operator_stencils(i-lb, j-lb);
 		}
 	}
 
@@ -58,7 +72,8 @@ SparseSolver::solve(Eigen::VectorXd& x, const Eigen::VectorXd& rhs) {
 		m_has_already_factorized = true;
 
 		if (m_sparse_solver.info() != Eigen::Success) {
-			std::runtime_error("LU decomposition failed");
+			assert(false);
+			throw std::runtime_error("LU decomposition failed");
 		}
 	}
 
